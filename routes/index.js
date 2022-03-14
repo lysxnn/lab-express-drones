292d0ae1286192c524755149101e4ef1e5807806
Lysann Kuettner <ly.sann@live.de>
2022-03-14 21:51:00 +0100

    not solved

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 /* GET home page */
+router.use(require("./drones"));
 router.get("/", (req, res, next) => res.render("index"));
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,24 @@
 // Iteration #1
+require("../db");
+const Drone = require("../models/Drone.model");
+const mongoose = require("mongoose");
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+];
+
+async function main() {
+  try {
+    await Drone.create(drones);
+    console.log("All the drones were seeded.");
+  } catch (err) {
+    console.log("There was a mistake seeding.");
+  } finally {
+    mongoose.disconnect();
+    console.log("The connection is closed now.");
+  }
+}
+
+main();
